5cad8d28bb783e651935dd6a6d410b0a9f5e7808
liyunhongliyun <li2013087636@qq.com>
2024-05-23 21:47:29 +0800

    Create talk_room.sql

---
+++ SQL/talk_room.sql
@@ -0,0 +1,5 @@
+INSERT INTO `talk_room`(`id`, `used`, `large`, `opentime`, `closetime`, `equipment`) VALUES ('101', '可用', 10, '07:30:00', '23:00:00', '是');
+INSERT INTO `talk_room`(`id`, `used`, `large`, `opentime`, `closetime`, `equipment`) VALUES ('102', '可用', 15, '07:30:00', '23:00:00', '是');
+INSERT INTO `talk_room`(`id`, `used`, `large`, `opentime`, `closetime`, `equipment`) VALUES ('201', '不可用', 10, '08:30:00', '17:00:00', '否');
+INSERT INTO `talk_room`(`id`, `used`, `large`, `opentime`, `closetime`, `equipment`) VALUES ('202', '可用', 20, '09:30:00', '17:00:00', '是');
+INSERT INTO `talk_room`(`id`, `used`, `large`, `opentime`, `closetime`, `equipment`) VALUES ('301', '可用', 5, '07:30:00', '19:00:00', '是');
